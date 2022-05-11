aea56489b1a97fd9fc35d41bd0f978bb4acc1158
SergeyPlatinov <slplatinov@yandex.ru>
2022-05-11 23:58:29 +0300

    лаболаторная работа №12 (переключение мужду активностями)

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.view.View.OnClickListener;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+    Button btnActTwo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnActTwo = (Button) findViewById(R.id.btnActTwo);
+        btnActTwo.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnActTwo:
+                Intent intent = new Intent(this, ActivityTwo.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
